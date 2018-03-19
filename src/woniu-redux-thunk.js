b1a8c19d7e22fadd139301f7f04462efab9c7aa5
ifyour <ifyour@outlook.com>
2018-03-19 17:32:01 +0800

    add comments

--- src/woniu-redux-thunk.js
+++ src/woniu-redux-thunk.js
@@ -2,11 +2,12 @@
  * 处理异步 action 中间件实现
  */
 // next 下一个中间件, 其实就是穿进来的 store.dispatch
-const thunk = ({dispatch,getState})=>next=>action=>{
-// 这里做一次判断, 看是否是函数, 如果是函数, 那么把 dispatch 的能力交给函数内部处理, 通常用来处理异步
-  if (typeof action==='function') {
-		return action(dispatch,getState)
+const thunk = ({ dispatch, getState }) => next => action => {
+	// 这里做一次判断, 看是否是函数, 如果是函数, 那么把 dispatch 的能力交给函数内部处理, 通常用来处理异步
+	if (typeof action === 'function') {
+		return action(dispatch, getState)
 	}
+	// 不是函数, 那么直接 dispatch
 	return next(action)
 }
 
